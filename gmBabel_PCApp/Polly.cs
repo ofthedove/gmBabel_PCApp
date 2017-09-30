6dd97865f8290c161b5fdee9b55812321bd55b2a
Andrew Combs <andrewcombs13@gmail.com>
2017-09-30 18:42:21 -0400

    Got audio files downloading and saving. UI still screwy

--- gmBabel_PCApp/Polly.cs
+++ gmBabel_PCApp/Polly.cs
@@ -9,13 +9,16 @@ public class Polly
     SynthesizeSpeechRequest sreq;
     SynthesizeSpeechResponse sres;
 
+    string dir = "mp3\\";
+
     public Polly()
 	{
-        Amazon.Runtime.AWSCredentials credentials = new Amazon.Runtime.StoredProfileAWSCredentials("GMBabel");
-        AWSPollyClient = new AmazonPollyClient();
+        //Amazon.Runtime.AWSCredentials credentials = new Amazon.Runtime.StoredProfileAWSCredentials("GMBabel");
+        AWSPollyClient = new AmazonPollyClient("AKIAIGTMGWHSOZMIUWNA", "y7ruHdTrXgu77GJEOlcBcZz/cOpx7PJqFXRguxV4", Amazon.RegionEndpoint.USWest2);
         sreq = new SynthesizeSpeechRequest();
         sreq.OutputFormat = OutputFormat.Mp3;
         sreq.VoiceId = VoiceId.Amy;
+        sreq.TextType = TextType.Text;
     }
 
     public string sendText(string UIString)
@@ -25,15 +28,24 @@ public class Polly
 
         string fileName = mp3Name();
         fileName = fileName + ".mp3";
-        using (var fileStream = File.Create("/mp3/" + fileName));
+        using (var fileStream = File.Create(dir + fileName))
+        {
+            sres.AudioStream.CopyTo(fileStream);
+            fileStream.Flush();
+            fileStream.Close();
+        }
         return fileName;
     }
 
     private string mp3Name()
     {
-        string [] fileArray =Directory.GetFiles("/mp3");
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+        string [] fileArray =Directory.GetFiles(dir);
         
-        if(fileArray == null)
+        if(fileArray.Length < 1)
         {
             return "0001";
         }
@@ -41,6 +53,7 @@ public class Polly
         {
             Array.Sort(fileArray);
             string tempString = fileArray[fileArray.Length - 1];
+            tempString = tempString.Split('\\')[1];
             tempString = tempString.Remove(tempString.Length - 4);
             int tempInteger = Int32.Parse(tempString) +1;
             string returnString = tempInteger.ToString();
